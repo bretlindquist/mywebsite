9ddab08abbd7b7eb0e8413fa5ad7b53e6f821dc8
Bret Lindquist <30852056+bretlindquist@users.noreply.github.com>
2025-02-12 18:47:31 +0900

    lightbox refix

--- src/components/LightboxComponent.tsx
+++ src/components/LightboxComponent.tsx
@@ -1,32 +1,51 @@
 // src/components/LightboxComponent.tsx
-
-import React, { useEffect } from 'react';
-import GLightbox from 'glightbox';
+import React, { useEffect, useRef } from 'react';
+import dynamic from 'next/dynamic';
 
 interface LightboxComponentProps {
-  imageUrl: string;       // Or whatever type is appropriate
-  thumbnailUrl: string;   // Or whatever type is appropriate
-  alt?: string;          // Optional string
+    imageUrl: string;
+    thumbnailUrl: string;
+    alt?: string;
 }
 
+const GLightbox = dynamic(
+    () => import('glightbox').then((module) => module.default),
+    { ssr: false } // Disable server-side rendering for this component
+);
+
 const LightboxComponent: React.FC<LightboxComponentProps> = ({ imageUrl, thumbnailUrl, alt }) => {
-  useEffect(() => {
-    const lightbox = GLightbox({
-      selector: '.glightbox'
-    });
+    const lightboxRef = useRef(null);
 
-    return () => {
-      lightbox.destroy();
-    };
-  }, []);
+    useEffect(() => {
+        let lightboxInstance: any = null; // Type as any for now
 
-  return (
-    <div>
-      <a href={imageUrl} className="glightbox">
-        <img src={thumbnailUrl} alt={alt} />
-      </a>
-    </div>
-  );
+        const initializeLightbox = async () => {
+            if (typeof window !== 'undefined') {
+                const GLightboxModule = await import('glightbox');
+                const GLightbox = GLightboxModule.default;
+
+                lightboxInstance = GLightbox({
+                    selector: '.glightbox',
+                });
+            }
+        };
+
+        initializeLightbox();
+
+        return () => {
+            if (lightboxInstance) {
+                lightboxInstance.destroy();
+            }
+        };
+    }, []);
+
+    return (
+        <div>
+            <a href={imageUrl} className="glightbox" ref={lightboxRef}>
+                <img src={thumbnailUrl} alt={alt} />
+            </a>
+        </div>
+    );
 };
 
 export default LightboxComponent;
